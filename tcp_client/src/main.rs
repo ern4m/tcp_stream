2ecf2e99900d4947def04d8098f3e11f1f4c1c50
ern4m <ernam.rsf@gmail.com>
2023-06-02 11:58:48 -0300

    Atualizado o client (more todo)

--- tcp_client/src/main.rs
+++ tcp_client/src/main.rs
@@ -1,35 +1,43 @@
-use std::net::{TcpStream};
-use std::io::{Read, Write};
-use std::str::from_utf8;
+use std::io::{self, Read, Write};
+use std::net::TcpStream;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
-    while match TcpStream::connect("localhost:3333") {
-        Ok(mut stream) => {
-            println!("Successfully connected to server in port 3333");
+    // Connect to the server
+    let stream = TcpStream::connect("localhost:3333").expect("Failed to connect to server");
+    let stream = Arc::new(Mutex::new(stream));
 
-            let msg = b"I'm a tcp client!";
+    // Clone the stream for use in the receive thread
+    let receive_stream = Arc::clone(&stream);
 
-            stream.write(msg).unwrap();
-            println!("Sent Hello, awaiting reply...");
-
-            let mut data = [0 as u8; 17]; // using 6 byte buffer
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    if &data == msg {
-                        println!("Reply is ok!");
-                    } else {
-                        let text = from_utf8(&data).unwrap();
-                        println!("Unexpected reply: {}", text);
-                    }
-                },
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
-                }
+    // Create a separate thread to receive server responses
+    let receive_thread = thread::spawn(move || {
+        let mut buffer = [0; 1024];
+        loop {
+            let mut stream = receive_stream.lock().expect("Failed to acquire lock on stream");
+            let bytes_read = stream.read(&mut buffer).expect("Failed to read from server");
+            if bytes_read == 0 {
+                // Connection closed by the server
+                break;
             }
-        },
-        Err(e) => {
-            println!("Failed to connect: {}", e);
+            let response = String::from_utf8_lossy(&buffer[..bytes_read]);
+            println!("Server response: {}", response);
         }
-    } {}
-    println!("Terminated.");
+    });
+
+    // Main thread to send user input to the server
+    loop {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read user input");
+
+        let mut stream = stream.lock().expect("Failed to acquire lock on stream");
+        stream
+            .write(input.trim().as_bytes())
+            .expect("Failed to write to server");
+    }
+
+    receive_thread.join().expect("Receive thread panicked");
 }
